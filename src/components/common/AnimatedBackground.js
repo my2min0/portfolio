631d0d2e776e5efe441df58f7fe84cd1cde0a1e3
my2min0 <ffrf1234tina@gmail.com>
2025-06-09 00:18:59 +0900

    상태추가

--- src/components/common/AnimatedBackground.js
+++ src/components/common/AnimatedBackground.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AnimatedBackground = ({ children }) => {
+    const [ circles, setCircles ] = useState([]);
+    const [ isInitialLoad, setIsInitialLoad ] = useState(true);
+
     return (
         <div className="relative min-h-screen">
             <div className="relative z-10">
