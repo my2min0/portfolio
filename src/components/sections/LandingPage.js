3bbeb3fc0c86ccf5aa3f7252cba8b93d584ae6d7
my2min0 <ffrf1234tina@gmail.com>
2025-06-08 23:03:01 +0900

    useRef 추가

--- src/components/sections/LandingPage.js
+++ src/components/sections/LandingPage.js
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, { useState, useRef } from "react";
 
 const LandingPage = () => {
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-    const [isHovering, setIsHovering] = useState(false);
-    
+    const [ mousePosition, setMousePosition ] = useState({ x: 0, y: 0 });
+    const [ isHovering, setIsHovering ] = useState(false);
+    const textRef = useRef(null);
+
     return (
         <div className="min-h-screen">
             <h1>Front-End</h1>
