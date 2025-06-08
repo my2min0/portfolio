36d08c264b871ad0aa4ce243d5ce18cd0c795500
my2min0 <ffrf1234tina@gmail.com>
2025-06-08 23:51:48 +0900

    tailwind css 요소 추가
    - 마스크 효과 부드럽지 않음
    - 서브 타이틀과 메인 타이틀이 겹치지 않음

--- src/components/sections/LandingPage.js
+++ src/components/sections/LandingPage.js
@@ -38,23 +38,42 @@ const LandingPage = () => {
             : 'none',
     }
 
+    // Front-End 타이틀 랜더링 함수
+    const renderMainTitle = () => {
+        return (
+            <h1 className="font-black text-6xl md:text8xl lg:text-9xl tracking-tighter">
+                Front-End
+            </h1>
+        );
+    };
+
     return (
-        <div className="min-h-screen">
-            <div className="relative"
-                onMouseMove={ handleMouseMove }
-                onMouseEnter={ handleMouseEnter }
-                onMouseLeave={ handleMouseLeave }
-            >
-                {/* 첫 번째 레이어 */}
-                <h1 className="text-pink-100">
-                    Front-End
-                </h1>
+        <div className="relative flex flex-col items-center justify-center min-h-screen px-4">
+            <div className="max-w-4xl mx-auto text-center">
+                {/* Front-End 큰 제목 */}
+                <div className="relative cursor-pointer mb-8 lg:mb-1"
+                    onMouseMove={ handleMouseMove }
+                    onMouseEnter={ handleMouseEnter }
+                    onMouseLeave={ handleMouseLeave }
+                >
+                    {/* 첫 번째 레이어 */}
+                    <div className="text-pink-100 leading-none select-none">
+                        { renderMainTitle() }
+                    </div>
 
-                {/* 두 번째 레이어 */}
-                <h1 className="absolute top-0 left-0 text-pink" style={hoverTextStyle}>
-                    Front-End
-                </h1>
+                    {/* 두 번째 레이어 : 마우스 위치에 따라 마스크 효과 적용 */}
+                    <div className="absolute text-pink-400 top-0 left-0 leading-none transition-opacity duration-300 select-none" style={ hoverTextStyle }>
+                        { renderMainTitle() }
+                    </div>
+                </div>
 
+                {/* 서브 타이틀 */}
+                <div className="text-black font-medium">
+                    <p className="text-lg md:text-xl lg:text-2xl leading-relaxed">
+                        문제 해결 능력을 갖춘 개발자 <b>이민영</b>입니다
+                    </p>
+
+                </div>
             </div>
         </div>
     );
