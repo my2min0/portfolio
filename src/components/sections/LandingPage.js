57197846e430d7671bd1291a1d97265492a6e0b9
my2min0 <ffrf1234tina@gmail.com>
2025-06-08 23:06:35 +0900

    이벤트 핸들러 추가 & JSX에 이벤트 연결

--- src/components/sections/LandingPage.js
+++ src/components/sections/LandingPage.js
@@ -4,10 +4,29 @@ const LandingPage = () => {
     const [ mousePosition, setMousePosition ] = useState({ x: 0, y: 0 });
     const [ isHovering, setIsHovering ] = useState(false);
     const textRef = useRef(null);
+    
+    // 마우스 이벤트 핸들러
+    const handleMouseMove = (e) => {
+        console.log('마우스 움직임!', e.clientX, e.clientY);
+    };
+    const handleMouseEnter = () => {
+        console.log('마우스 들어옴!');
+        setIsHovering(true);
+    };
+    const handleMouseLeave = () => {
+        console.log('마우스 나감!');
+        setIsHovering(false);
+    };
 
     return (
         <div className="min-h-screen">
-            <h1>Front-End</h1>
+            <div
+                onMouseMove={ handleMouseMove }
+                onMouseEnter={ handleMouseEnter }
+                onMouseLeave={ handleMouseLeave }
+            >
+                <h1>Front-End</h1>
+            </div>
         </div>
     );
 };
