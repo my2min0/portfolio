e36e9fb1b86920ef08470df688ab8b6fcfd5b6a3
my2min0 <ffrf1234tina@gmail.com>
2025-06-09 21:56:41 +0900

    서브타이틀 슬라이드 효과

--- src/components/sections/LandingPage.js
+++ src/components/sections/LandingPage.js
@@ -4,6 +4,19 @@ import MaskText from "../common/MaskText";
 const LandingPage = () => {
     return (
         <div className="relative flex flex-col items-center justify-center min-h-screen px-4">
+            <style jsx>{`
+                @keyframes slideUp {
+                    to {
+                        opacity: 1;
+                        transform: translateY(0);
+                    }
+                }
+                .slide-up {
+                    opacity: 0;
+                    transform: translateY(30px);
+                    animation: slideUp 1.5s ease-out 1s forwards;
+                }
+            `}</style>
             <div className="max-w-4xl mx-auto text-center">
                 {/* Front-End 큰 제목 */}
                 <MaskText
@@ -16,7 +29,7 @@ const LandingPage = () => {
                 </MaskText>
 
                 {/* 서브 타이틀 */}
-                <div className="relative text-black font-medium z-20">
+                <div className="relative text-black font-medium z-20 slide-up">
                     <p className="text-lg md:text-3xl leading-relaxed -mt-14 md:-mt-10">
                         문제 해결 능력을 갖춘 개발자 <b>이민영</b>입니다
                     </p>
