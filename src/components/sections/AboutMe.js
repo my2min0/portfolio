9a854c981b736d6c5bc8ebb2def1d3dfed090606
my2min0 <ffrf1234tina@gmail.com>
2025-06-09 14:36:02 +0900

    해시태그 배열 생성 및 랜더링

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const AboutMe = () => {
+    const hashtags = [
+        "#문제_해결_능력",
+        "#리더십",
+        "#협업_능력", 
+        "#풀스택_교육_이수",
+        "#책임감"
+    ];
+
     return (
         <div className="min-h-screen flex flex-col item-center pt-20 px-4">
             <h1>AboutMe</h1>
@@ -12,8 +20,10 @@ const AboutMe = () => {
             </div>
 
             {/* 해시태그 */}
-            <div>
-                해시태그
+            <div className="flex flex-wrap justify-center gap-4 mb-12 max-w-2xl">
+                {hashtags.map((tag, index) => (
+                    <span key={index}>{tag}</span>
+                ))}
             </div>
 
             {/* 팀원 리뷰 버튼 */}
