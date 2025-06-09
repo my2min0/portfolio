85845a209de53b49ee128806a03b77cd47e1cfd2
my2min0 <ffrf1234tina@gmail.com>
2025-06-09 19:21:51 +0900

    메인 타이틀 크기 변경

--- src/components/sections/LandingPage.js
+++ src/components/sections/LandingPage.js
@@ -10,14 +10,14 @@ const LandingPage = () => {
                     className="mb-8 lg:mb-1 z-10"
                     maskSize={200}
                 >
-                    <h1 className="text-8xl lg:text-[10rem] tracking-tighter font-black">
+                    <h1 className="text-7xl lg:text-[10rem] tracking-tighter font-black">
                         Front-End
                     </h1>
                 </MaskText>
 
                 {/* 서브 타이틀 */}
                 <div className="relative text-black font-medium z-20">
-                    <p className="text-xl lg:text-3xl leading-relaxed -mt-14 lg:-mt-10">
+                    <p className="text-lg lg:text-3xl leading-relaxed -mt-14 lg:-mt-10">
                         문제 해결 능력을 갖춘 개발자 <b>이민영</b>입니다
                     </p>
                 </div>
