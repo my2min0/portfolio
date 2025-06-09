494e04755ea5fd496dca848698acfca703b87e1e
my2min0 <ffrf1234tina@gmail.com>
2025-06-09 13:48:25 +0900

    AboutMe 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import LandingPage from './components/sections/LandingPage';
 import AnimatedBackground from './components/common/AnimatedBackground';
+import AboutMe from './components/sections/AboutMe';
 
 const App = () => {
   return (
     <div className="App">
       <AnimatedBackground>
         <LandingPage />
-
+        <AboutMe />
       </AnimatedBackground>
     </div>
   );
