c461a7d7718d6811db007041fd2ca631da528a8a
my2min0 <ffrf1234tina@gmail.com>
2025-06-10 17:42:57 +0900

    클로버 아이콘으로 변경

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -40,15 +40,17 @@ const AboutMe = () => {
     return (
         <div className="min-h-screen md:min-h-[120vh] flex flex-col pt-32 md:pt-20 px-4">
             <div className="flex">
-                <h3 className="text-3xl mt-2 mr-3 md:text-5xl md:mt-4">🍀 </h3>
                 {/* 상단 제목 - 마스크 효과 */}
                 <MaskText 
                     className="mb-12 text-left z-10"
                     maskSize={100}
                 >
-                    <h1 className="text-5xl md:text-7xl font-black">
-                        About Me
-                    </h1>
+                    <div className="flex items-center gap-3 ml-2 md:ml-7">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clover-icon lucide-clover"><path d="M16.17 7.83 2 22"/><path d="M4.02 12a2.827 2.827 0 1 1 3.81-4.17A2.827 2.827 0 1 1 12 4.02a2.827 2.827 0 1 1 4.17 3.81A2.827 2.827 0 1 1 19.98 12a2.827 2.827 0 1 1-3.81 4.17A2.827 2.827 0 1 1 12 19.98a2.827 2.827 0 1 1-4.17-3.81A1 1 0 1 1 4 12"/><path d="m7.83 7.83 8.34 8.34"/></svg>
+                        <h1 className="text-5xl md:text-7xl font-black">
+                            About Me
+                        </h1>
+                    </div>
                 </MaskText>
             </div>
 
