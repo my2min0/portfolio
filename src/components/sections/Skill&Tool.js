fbd9862b1bb6c6f5dd137d3aa1b06fa6c01116a5
my2min0 <ffrf1234tina@gmail.com>
2025-06-17 00:37:20 +0900

    모바일에서 아이콘이 넘 크게 나와서 수정

--- src/components/sections/Skill&Tool.js
+++ src/components/sections/Skill&Tool.js
@@ -11,7 +11,7 @@ const SkillAndTool = () => {
         {
             id: 1,
             title: "Programming Languages & Database",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 md:w-[30px] md:h-[30px]" viewBox="0 0 24 24" fill="none" stroke="#2E6F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code-icon lucide-code"><path d="m16 18 6-6-6-6"/><path d="m8 6-6 6 6 6"/></svg>,
+            icon: <svg xmlns="http://www.w3.org/2000/svg" className="w-[24px] h-[24px] md:w-[30px] md:h-[30px]" viewBox="0 0 24 24" fill="none" stroke="#2E6F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code-icon lucide-code"><path d="m16 18 6-6-6-6"/><path d="m8 6-6 6 6 6"/></svg>,
             proficient: ["Java", "JavaScript", "Oracle SQL"],
             familiar: ["Python"],
             tried: ["MyBatis", "JDBC", "JPA"]
@@ -19,7 +19,7 @@ const SkillAndTool = () => {
         {
             id: 2,
             title: "Framework & Library",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 md:w-[30px] md:h-[30px]" viewBox="0 0 24 24" fill="none" stroke="#2E6F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>,
+            icon: <svg xmlns="http://www.w3.org/2000/svg" className="w-[24px] h-[24px] md:w-[30px] md:h-[30px]" viewBox="0 0 24 24" fill="none" stroke="#2E6F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>,
             proficient: ["React.js", "Vue.js", "HTML", "CSS", "Spring Boot"],
             familiar: ["JSP", "Servlet"],
             tried: ["JWT"]
@@ -27,7 +27,7 @@ const SkillAndTool = () => {
         {
             id: 3,
             title: "Development Tools & Collaboration",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 md:w-[30px] md:h-[30px]" viewBox="0 0 24 24" fill="none" stroke="#2E6F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wrench-icon lucide-wrench"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/></svg>,
+            icon: <svg xmlns="http://www.w3.org/2000/svg" className="w-[24px] h-[24px] md:w-[30px] md:h-[30px]" viewBox="0 0 24 24" fill="none" stroke="#2E6F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wrench-icon lucide-wrench"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/></svg>,
             proficient: ["GitHub", "Notion", "Visual Studio Code", "IntelliJ IDEA", "ERD Cloud", "SQL Developer"],
             familiar: ["Figma", "Eclipse"],
             tried: ["DBeaver", "Swaggeer"]
