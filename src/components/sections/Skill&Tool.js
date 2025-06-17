791ef3ba7c3d1669153646f7e9078ab2ec252595
my2min0 <ffrf1234tina@gmail.com>
2025-06-17 10:53:36 +0900

    모든 카드에 가이드 배경 나타나도록 변경!

--- src/components/sections/Skill&Tool.js
+++ src/components/sections/Skill&Tool.js
@@ -174,7 +174,7 @@ const SkillAndTool = () => {
                                 </div>
 
                                 {/* 안내 오버레이 */}
-                                { showGuide && index === currentIndex && (
+                                { showGuide && (
                                     <div className="absolute inset-0 bg-[#A7D9C4] bg-opacity-90 rounded-xl flex items-center justify-center z-40 gap-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pointer-icon lucide-pointer"><path d="M22 14a8 8 0 0 1-8 8"/><path d="M18 11v-1a2 2 0 0 0-2-2a2 2 0 0 0-2 2"/><path d="M14 10V9a2 2 0 0 0-2-2a2 2 0 0 0-2 2v1"/><path d="M10 9.5V4a2 2 0 0 0-2-2a2 2 0 0 0-2 2v10"/><path d="M18 11a2 2 0 1 1 4 0v3a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L7 15"/></svg>
                                         <p className="text-white text-lg md:text-2xl font-semibold">
