a466c091bf969e76c0b4635d98487abaf748aecf
my2min0 <ffrf1234tina@gmail.com>
2025-06-24 18:12:11 +0900

    임시로 아이콘 넣어둠 위치도 임시로 넣어둠

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -2,8 +2,35 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>
-            <p>CONTACT ME !</p>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100 p-8">
+            <div className="relative grid grid-cols-12 grid-rows-5 gap-0 items-center justify-center">
+                {/* 초록색 클로버 */}
+                <div className="col-span-4 row-start-1 flex justify-center items-end pb-2">
+                    <img 
+                        src="/images/clover.png" alt="clover" 
+                        className="object-contain"
+                    />
+                </div>
+
+                {/* 빨간색 하트 */}
+                <div className="col-span-4 col-start-5 row-start-1 flex justify-center items-end pb-2">
+                    <img 
+                        src="/images/heart.png" alt="heart" 
+                        className="object-contain"
+                    />
+                </div>
+
+                {/* 하늘색 돌 */}
+                <div className="col-span-4 col-start-4 row-start-2 flex justify-center items-start">
+                    <img 
+                        src="/images/rock.png" alt="rock" 
+                        className="object-contain"
+                    />
+                </div>
+            </div>
+            {/* <div className="col-span-12 row-span-3 row-start-2 flex items-center justify-center">
+                <h1 className="font-black text-[100pt] text-gray-400 tracking-wide text-center">CONTACT ME !</h1>
+            </div> */}
         </div>
     )
 }
