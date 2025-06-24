d6f93b100776eb7f04e16d48aefa05a32ea53143
my2min0 <ffrf1234tina@gmail.com>
2025-06-24 15:27:39 +0900

    프로젝트 카드 사이즈 조정 완료!

--- src/components/common/ProjectCard.js
+++ src/components/common/ProjectCard.js
@@ -5,14 +5,14 @@ const ProjectCard = ({ imgSrc, imgAlt, title }) => {
     return (
         <div className="flex justify-center">
         <div className="cursor-pointer transform transition-transform duration-300 hover:scale-[102%]">
-            <div className="w-[320px] md:w-[400px] xl:w-[520px] bg-white drop-shadow-xl rounded-2xl m-5">
+            <div className="w-[360px] xl:w-[520px] bg-white drop-shadow-xl rounded-2xl m-5">
                 {/* 맥북 사파리 st 헤더 */}
                 <div className="bg-gray-200 rounded-t-2xl p-2 pl-3 w-full group flex items-center gap-2">
                     <div className="relative w-3 h-3 rounded-full bg-red-400"/>
                     <div className="relative w-3 h-3 rounded-full bg-yellow-500"/>
                     <div className="relative w-3 h-3 rounded-full bg-green-500"/>
                 </div>
-                <div className="h-[200px] md:h-[250px] xl:h-[325px] overflow-hidden">
+                <div className="h-[225px] xl:h-[325px] overflow-hidden">
                     <img 
                         src={imgSrc} 
                         alt={imgAlt} 
