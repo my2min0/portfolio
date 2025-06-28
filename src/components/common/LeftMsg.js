d42fc397a4b79a9b0daef60188a5710683d29f2b
my2min0 <ffrf1234tina@gmail.com>
2025-06-28 14:54:54 +0900

    메시지 모달 프로필 삭제

--- src/components/common/LeftMsg.js
+++ src/components/common/LeftMsg.js
@@ -3,7 +3,6 @@ import React from "react";
 const LeftMsg = ({ message }) => {
     return (
         <div className="flex justify-start gap-2 mb-2">
-            <img src="/images/Clover.png" alt="profile" className="w-10 h-10 rounded-full bg-white-200"/>
             <div className="relative max-w-xs bg-white/90 p-3 text-base text-gray-800 mt-2 px-4 py-2 rounded-2xl rounded-tl-none shadow">
                 {message}
             </div>
