b7551c9a500b466c0490163e82e82d3a854237f3
my2min0 <ffrf1234tina@gmail.com>
2025-06-28 15:00:59 +0900

    프로젝트 카드 아래 여백 조정

--- src/components/sections/Project.js
+++ src/components/sections/Project.js
@@ -44,7 +44,7 @@ const Project = () => {
     }
 
     return (
-        <div className="min-h-[100vh] flex flex-col pt-32 md:pt-20 px-4 pb-20">
+        <div className="min-h-[90vh] flex flex-col pt-32 md:pt-20 px-4 pb-20">
             <div className="flex">
                 {/* 상단 제목 - 마스크 효과 */}
                 <MaskText 
