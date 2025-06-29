09ce3893cc6d45705f649d794d403ad6167033d6
my2min0 <ffrf1234tina@gmail.com>
2025-06-29 14:16:11 +0900

    프레임워크 추가

--- src/components/sections/Skill&Tool.js
+++ src/components/sections/Skill&Tool.js
@@ -21,8 +21,8 @@ const SkillAndTool = () => {
             id: 2,
             title: "Framework & Library",
             icon: <svg xmlns="http://www.w3.org/2000/svg" width="24px" viewBox="0 0 24 24" fill="none" stroke="#2E6F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>,
-            proficient: ["React.js", "Vue.js", "Spring Boot"],
-            familiar: ["JSP", "Servlet"],
+            proficient: ["React.js", "Vue.js", "Spring Boot", "Tailwind CSS"],
+            familiar: ["JSP", "Servlet", "Bootstrap"],
             tried: ["MyBatis", "JWT", "JPA"]
         },
         {
